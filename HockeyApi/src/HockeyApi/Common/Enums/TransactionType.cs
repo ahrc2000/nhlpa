f411b6cf29c71461aecc8659fc70ad655244b0f2
ahrc2000 <ali.chaudhry@hellogbye.com>
2020-10-04 00:45:03 -0400

    First Commit

---
+++ HockeyApi/src/HockeyApi/Common/Enums/TransactionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HockeyApi.Common.Enums
+{
+    public enum TransactionType
+    {
+        Signed =1,
+        Injured  =2 ,
+        Healthy =3,
+        Traded  = 4 
+    }
+}
---
+++ HockeyApi/src/HockeyApi/Features/Team/TeamController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HockeyApi.Features {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TeamController : Controller {
+		private readonly ITeamService _service;
+
+		public TeamController(ITeamService service) {
+			_service = service;
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<TeamModel>> Get()
+        {
+			return _service.List();
+        }
+
+
+		public IActionResult Index() => 
+			Json(_service.List());
+	}
+}
