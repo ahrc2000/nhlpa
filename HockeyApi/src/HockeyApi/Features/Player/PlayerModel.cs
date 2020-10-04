9ff235c99888074c2230302855862c680105a230
ahrc2000 <ali.chaudhry@hellogbye.com>
2020-10-04 00:49:36 -0400

    organizing the code and adding in the required features

---
+++ HockeyApi/src/HockeyApi/Features/Player/PlayerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HockeyApi.Features.Player
+{
+    public class PlayerModel
+    {
+    }
+}
